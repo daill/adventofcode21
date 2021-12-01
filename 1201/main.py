134e587f99093dbfeec928a67fdad332eaa10af8
Christian Kramer <daill@detx.fritz.box>
2021-12-01 21:38:27 +0100

    second part

--- 1201/main.py
+++ 1201/main.py
@@ -1,16 +1,24 @@
 import os
 
+
 def measure():
     count = 0
     with open(os.path.abspath('./1201/depths')) as file:
         lines = file.readlines()
-        prevDepth = 0
+        window = []
         for line in lines:
-            if prevDepth < int(line) and prevDepth != 0:
-                count += 1
-            prevDepth = int(line)
-
+            if len(window) < 4:
+                window.append(int(line))
+                print(str(window))
+            else:
+                print(str(window) + " " + str(sum(window)-window[3]) + "  " + str(sum(window)-window[0]))
+                if (sum(window)-window[3]) < (sum(window)-window[0]):
+                    count += 1
+                del window[0]
+                window.insert(3, int(line))
         return count
+
+
     
 
 if __name__ == '__main__':
